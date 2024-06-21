2a12242d787683350a582ab4aa7f20c4314bee27
Bereket Abera <Bereket.Abera@zemenbank.local>
2024-06-21 14:27:45 +0300

    Creating/Adding a data to the Server

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import axios, { AxiosError, CanceledError } from 'axios'
+import axios from 'axios'
 
 interface User{
   id: number,
@@ -9,6 +9,7 @@ const App = () => {
   const [user, setUser]=useState<User[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(false);
+  const existingUser = [...user]
   useEffect(()=>{
     // const controller = new AbortController();
 
@@ -24,27 +25,41 @@ const App = () => {
       setError(err.message),
       setLoading(false)
     })
-    // .finally(()=> setLoading(false)) Won't work in Strict Mode Enabled WHy?
-    // Even Mosh didn't know that
-
-    // const fetchUser = async() => {
-    //   try{
-    //     const res = await axios.get<User[]>('https://jsonplaceholder.typicode.com/xusers')
-    //   setUser(res.data)
-    //   }catch(err){
-    //     setError((err as AxiosError).message)
-    //   }
-    // }
-
-    // fetchUser()
-    // return () => controller.abort()
+    
   }, [])
+
+  const deleteUser = (users : User) => {
+  const filteredUser =user.filter(u => u.id !== users.id)
+  axios.delete('https://jsonplaceholder.typicode.com/users/' + users.id)
+  .catch(error => {
+    setError(error.message)
+    setUser(existingUser)
+  })
+  setUser(filteredUser)
+
+   
+  }
+
+  const addUser =() =>{
+    const newUser = {id:0, name: "Becki"}
+    setUser([newUser,...user])
+    axios.post('https://jsonplaceholder.typicode.com/users/', newUser)
+    .then(({data: NewUser}) => setUser([NewUser,...user]))
+    .catch((error) => {
+      setError(error.message)
+      setUser(existingUser)
+    })
+  }
   return (
     <>
     
     {isLoading && <div className="spinner-border"></div>}
     {error && <p className="text-danger">{error}</p>}
-  <ul>{user.map(user =><li key={user.id}>{user.id} {user.name}</li>) }</ul>
+    <button className="btn btn-primary mb-3" onClick={addUser}>Add User</button>
+    <ul className="list-group">
+    {user.map(user =><li className="list-group-item d-flex justify-content-between" key={user.id}>{user.name} <button className="btn btn-outline-danger " onClick={() => deleteUser(user)}>Delete</button></li>) }
+    </ul>
+
 
     </>
   )
